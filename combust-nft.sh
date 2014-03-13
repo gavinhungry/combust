6724002382dffed9b2d647bace51dd549ad74098
Gavin Lloyd <gavinhungry@gmail.com>
2014-03-12 17:01:38 -0700

    Update remainder of combust-nft

--- combust-nft.sh
+++ combust-nft.sh
@@ -5,16 +5,15 @@
 # Date: 06 Mar 2014 (last modified: 12 Mar 2014)
 # Desc: nftables-based firewall script with simple profiles
 #
-# THIS SCRIPT IS INCOMPLETE
-#
 
 NFT=/usr/bin/nft
+IP=/usr/bin/ip
 
 declare -A IF
 declare -A CLIENTS
 
 # FIXME: /etc/combust/combust.conf
-source ./combust-nft.conf
+source ./combust.conf
 
 ERRORS=0
 for PARM in "$@"; do
@@ -56,17 +55,16 @@ nft4() {
 }
 
 nft6() {
-  # FIXME
-  # pref USE_IPV6 || return 0
+  pref USE_IPV6 || return 0
   nft "ip6" "$@"
 }
 
 nft4chain() {
-  nft4 add chain filter "$@"
+  nft4 add chain "$@"
 }
 
 nft6chain() {
-  nft6 add chain filter "$@"
+  nft6 add chain "$@"
 }
 
 nftchain() {
@@ -75,16 +73,39 @@ nftchain() {
 }
 
 nft4rule() {
-  nft4 add rule filter "$@"
+  nft4 add rule "$@"
 }
 
 nft6rule() {
-  nft6 add rule filter "$@"
+  nft6 add rule "$@"
 }
 
 nftrule() {
   nft4rule "$@"
   nft6rule "$@"
+}
+
+nftpolicy() {
+  TABLE=$1
+  CHAIN=$2
+  POLICY=$3
+
+  nft ip add rule $TABLE $CHAIN $POLICY
+
+  pref USE_IPV6 || POLICY=drop
+  nft ip6 add rule $TABLE $CHAIN $POLICY
+}
+
+_awksub_ip() {
+  awk '{sub(/\/.*/,"",$2); print $2}'
+}
+
+inet() {
+  $IP -4 addr show $1 | grep '^\s*inet\s' | _awksub_ip
+}
+
+inet6() {
+  $IP -6 addr show $1 | grep '^\s*inet6\s' | _awksub_ip
 }
 
 # ---[ FLUSH ]------------------------------------------------------------------
@@ -113,91 +134,94 @@ fi
 
 # input/output/forward chains on a filter table
 $NFT -f /etc/nftables/ipv4-filter
-$NFT -f /etc/nftables/ipv6-filter
+$NFT -f /etc/nftables/ipv4-nat
 
-nftchain valid_src
-nftchain valid_dst
+$NFT -f /etc/nftables/ipv6-filter
+$NFT -f /etc/nftables/ipv6-nat
+
+nftchain filter valid_src
+nftchain filter valid_dst
 
 # ---[ VALID ]------------------------------------------------------------------
 msg 'External interface sources'
 if [ ! -z "$IPV4_LAN" ]; then
   for RANGE in $IPV4_LAN; do
-    nft4rule valid_src ip saddr $RANGE return
+    nft4rule filter valid_src ip saddr $RANGE return
   done
 fi
 
 # RFC1918 private addresses - include in IPV4_LAN to allow
-nft4rule valid_src ip saddr 10.0.0.0/8     drop
-nft4rule valid_src ip saddr 172.16.0.0/12  drop
-nft4rule valid_src ip saddr 192.168.0.0/16 drop
+nft4rule filter valid_src ip saddr 10.0.0.0/8     drop
+nft4rule filter valid_src ip saddr 172.16.0.0/12  drop
+nft4rule filter valid_src ip saddr 192.168.0.0/16 drop
 
-nft4rule valid_src ip saddr 127.0.0.0/8 drop
-nft4rule valid_src ip saddr 169.254.0.0/16 drop
-nft4rule valid_src ip saddr 0.0.0.0/8 drop
-nft4rule valid_src ip saddr 255.255.255.255 drop
-nft4rule valid_src ip saddr 192.168.0.0/16 drop
+nft4rule filter valid_src ip saddr 127.0.0.0/8     drop
+nft4rule filter valid_src ip saddr 169.254.0.0/16  drop
+nft4rule filter valid_src ip saddr 0.0.0.0/8       drop
+nft4rule filter valid_src ip saddr 255.255.255.255 drop
+nft4rule filter valid_src ip saddr 192.168.0.0/16  drop
 
 if [ ! -z "$IPV6_LAN" ]; then
   for RANGE in $IPV6_LAN; do
-    nft6rule valid_src ip6 saddr $RANGE return
+    nft6rule filter valid_src ip6 saddr $RANGE return
   done
 fi
 
-nft6rule valid_src ip6 saddr ::1/128 drop
+nft6rule filter valid_src ip6 saddr ::1/128 drop
 
 msg 'External interface destinations'
-nft4rule valid_dst ip daddr 127.0.0.0/8 drop
-nft4rule valid_dst ip daddr 224.0.0.0/4 drop
+nft4rule filter valid_dst ip daddr 127.0.0.0/8 drop
+nft4rule filter valid_dst ip daddr 224.0.0.0/4 drop
 
-nft6rule valid_dst ip6 daddr ::1/128 drop
+nft6rule filter valid_dst ip6 daddr ::1/128 drop
 
 
 # ---[ INPUT ]------------------------------------------------------------------
 msg 'filter/input'
 
-nftrule input ct state invalid drop
-nftrule input ct state { related, established } accept
+nftrule filter input ct state invalid drop
+nftrule filter input ct state { related, established } accept
 
 # loopback
-nft4rule input iifname ${IF[LO]} ip saddr 127.0.0.0/8 ip daddr 127.0.0.0/8 accept
-nft6rule input iifname ${IF[LO]} ip6 saddr ::1/128 ip6 daddr ::1/128 accept
+nft4rule filter input iifname ${IF[LO]} ip saddr 127.0.0.0/8 ip daddr 127.0.0.0/8 accept
+nft6rule filter input iifname ${IF[LO]} ip6 saddr ::1/128 ip6 daddr ::1/128 accept
 
 msg 'filter/input: common attacks'
-nftchain syn_flood
-nftrule syn_flood limit rate 2/second accept # FIXME: burst of 4
-nftrule syn_flood accept
+nftchain filter syn_flood
+nftrule filter syn_flood limit rate 2/second accept # FIXME: burst of 4
+nftrule filter syn_flood accept
 
 for I in ${IF[WAN]}; do
   WAN=${IF[$I]-$I}
 
-  nftrule input iifname $WAN tcp flags '& (syn|rst|ack) == (syn)' jump syn_flood
+  nftrule filter input iifname $WAN tcp flags '& (syn|rst|ack) == (syn)' jump syn_flood
 
-  nftrule input iifname $WAN ct state new tcp flags '& (syn) < (syn)' drop
-  nftrule input iifname $WAN ct state new tcp flags '& (syn|rst) == (syn|rst)' drop
-  nftrule input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin|syn)' drop
-  nftrule input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin)' drop
-  nftrule input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) < (fin)' drop
-  nftrule input iifname $WAN ct state new tcp flags '== (fin|syn|rst|psh|ack|urg)' drop
-  nftrule input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin|psh|urg)' drop
-  nftrule input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin|syn|psh|urg)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (syn) < (syn)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (syn|rst) == (syn|rst)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin|syn)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) < (fin)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '== (fin|syn|rst|psh|ack|urg)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin|psh|urg)' drop
+  nftrule filter input iifname $WAN ct state new tcp flags '& (fin|syn|rst|psh|ack|urg) == (fin|syn|psh|urg)' drop
 
-  nftrule input iifname $WAN jump valid_src
+  nftrule filter input iifname $WAN jump valid_src
 done
 
 
 msg 'filter/input: allowed LAN traffic'
 if pref ROUTING; then
-  nftrule input iffname ${IF[LAN]} accept
+  nftrule filter input iffname ${IF[LAN]} accept
 fi
 
 # ICMP ping
 if pref ICMP_REPLY; then
-  nft4rule input icmp   type echo-request limit rate 8/second accept
-  nft6rule input icmpv6 type echo-request limit rate 8/second accept
+  nft4rule filter input icmp   type echo-request limit rate 8/second accept
+  nft6rule filter input icmpv6 type echo-request limit rate 8/second accept
 fi
 
 # IPv6
-nft6rule input ip6 saddr fe80::/10 icmpv6 type { nd-neighbor-solicit, nd-neighbor-advert } accept
+nft6rule filter input ip6 saddr fe80::/10 icmpv6 type { nd-neighbor-solicit, nd-neighbor-advert } accept
 
 msg 'filter/input: per-interface rules'
 for IL in ${!IF[@]}; do
@@ -210,91 +234,90 @@ for IL in ${!IF[@]}; do
           LIMIT=$(echo $PORT | cut -d':' -f2)
           BURST=$(echo $PORT | cut -d':' -f3)
           for P in $(eval echo "$DPORT"); do
-            nftrule input iifname ${IF[$I]-$I} ct state new ${PROTO,,} dport $P limit rate ${LIMIT:-8}/minute accept
+            nftrule filter input iifname ${IF[$I]-$I} ct state new ${PROTO,,} dport $P limit rate ${LIMIT:-8}/minute accept
           done
           continue
         fi
         for P in $(eval echo "$PORT"); do
-          nftrule input iifname ${IF[$I]-$I} ${PROTO,,} dport $P accept
+          nftrule filter input iifname ${IF[$I]-$I} ${PROTO,,} dport $P accept
         done
       done
     done
   done
 done
 
-nftrule input drop
 
 # ---[ OUTPUT ]-----------------------------------------------------------------
 msg 'filter/output'
 
-nftrule output ct state invalid drop
+nftrule filter output ct state invalid drop
 
 for I in ${IF[WAN]}; do
-  nftrule output oifname ${IF[$I]-$I} jump valid_dst
+  nftrule filter output oifname ${IF[$I]-$I} jump valid_dst
 done
 
-nftrule output accept
 
 # ---[ FORWARD ]----------------------------------------------------------------
 msg 'filter/forward'
 
-# ipt  -P FORWARD DROP
-# ipt6 -P FORWARD DROP
+nftrule filter forward ct state invalid drop
 
-# ipt  -A FORWARD -m conntrack --ctstate INVALID -j DROP
-# ipt6 -A FORWARD -m conntrack --ctstate INVALID -j DROP
+msg 'filter/forward: route forwarding'
+if pref ROUTING; then
+  nftrule filter forward ct state { related, established } accept
+  nftrule filter forward iifname ${IF[LAN]} oifname ${IF[LAN]} accept
 
-# msg 'filter/FORWARD: route forwarding'
-# if pref ROUTING; then
-#   ipt  -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-#   ipt6 -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+  for I in ${IF[WAN]}; do
+    nftrule filter forward iifname ${IF[LAN]} oifname ${IF[$I]-$I} accept
+  done
+fi
 
-#   ipt  -A FORWARD -i ${IF[LAN]} -o ${IF[LAN]} -j ACCEPT
-#   ipt6 -A FORWARD -i ${IF[LAN]} -o ${IF[LAN]} -j ACCEPT
+if pref VPN_SERVER; then
+  for I in ${IF[WAN]}; do
+    nft4rule filter forward iifname ${IF[$I]-$I} oifname ${IF[VPN]}   ip daddr $IPV4_VPN ct state { related, established } accept
+    nft4rule filter forward iifname ${IF[VPN]}   oifname ${IF[$I]-$I} ip saddr $IPV4_VPN accept
+  done
+fi
 
-#   for I in ${IF[WAN]}; do
-#     ipt  -A FORWARD -i ${IF[LAN]} -o ${IF[$I]-$I} -j ACCEPT
-#     ipt6 -A FORWARD -i ${IF[LAN]} -o ${IF[$I]-$I} -j ACCEPT
-#   done
-# fi
-
-# if pref VPN_SERVER; then
-#   for I in ${IF[WAN]}; do
-#     ipt  -A FORWARD -i ${IF[$I]-$I} -o ${IF[VPN]} -d $IPV4_VPN -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-#     ipt  -A FORWARD -i ${IF[VPN]} -o ${IF[$I]-$I} -s $IPV4_VPN -j ACCEPT
-#   done
-# fi
-
-# msg 'filter/FORWARD: client port forwards'
-# for CLIENT in ${!CLIENTS[@]}; do
-#   for PROTO in TCP UDP; do
-#     PROTO_CLIENT=${PROTO}_${CLIENT}
-#     for PORT in ${!PROTO_CLIENT}; do
-#       FROM=$(echo $PORT | cut -d':' -f1)
-#       DEST=$(echo $PORT | cut -d':' -f2)
-#       HOST=${CLIENTS[$CLIENT]}
-#       for I in ${IF[WAN]}; do
-#         ipt -t nat -A PREROUTING -i ${IF[$I]-$I} -p ${PROTO,,} --dport $FROM -j DNAT --to-destination $HOST:$DEST
-#         ipt        -A FORWARD    -i ${IF[$I]-$I} -p ${PROTO,,} --dport $DEST -d $HOST -j ACCEPT
-#       done
-#     done
-#   done
-# done
+msg 'filter/forward: client port forwards (IPv4 only)'
+for CLIENT in ${!CLIENTS[@]}; do
+  for PROTO in TCP UDP; do
+    PROTO_CLIENT=${PROTO}_${CLIENT}
+    for PORT in ${!PROTO_CLIENT}; do
+      FROM=$(echo $PORT | cut -d':' -f1)
+      DEST=$(echo $PORT | cut -d':' -f2)
+      HOST=${CLIENTS[$CLIENT]}
+      for I in ${IF[WAN]}; do
+        nft4rule nat prerouting iifname ${IF[$I]-$I} ${PROTO,,} dport $FROM dnat $HOST:$DEST
+        nft4rule filter input   iifname ${IF[$I]-$I} ${PROTO,,} dport $DEST ip daddr $HOST accept
+      done
+    done
+  done
+done
 
 
 # ---[ POSTROUTING ]------------------------------------------------------------
 msg 'nat/postrouting'
 
-# if pref VPN_SERVER; then
-#   for I in ${IF[WAN]}; do
-#     ipt -t nat -A POSTROUTING -o ${IF[$I]-$I} -s $IPV4_VPN -j MASQUERADE
-#   done
-# fi
+# FIXME: `inet <interface>` will not work with dynamically assigned addresses,
+# unless combust-nft is re-executed after the interface comes back up
+#
+# Is there any support for a masquerade target?
 
-# if pref ROUTING; then
-#   for I in ${IF[WAN]}; do
-#     ipt -t nat -A POSTROUTING -o ${IF[$I]-$I} -j MASQUERADE
-#   done
-# fi
+for I in ${IF[WAN]}; do
+  if pref VPN_SERVER; then
+    nft4rule nat postrouting oifname ${IF[$I]-$I} ip saddr $IPV4_VPN snat $(inet ${IF[$I]-$I})
+  fi
 
-# finish
+  if pref ROUTING; then
+    nft4rule nat postrouting oifname ${IF[$I]-$I} snat $(inet ${IF[$I]-$I})
+  fi
+done
+
+# ---[ POLICY ]-----------------------------------------------------------------
+msg 'default chain policies'
+nftpolicy filter input drop
+nftpolicy filter output accept
+nftpolicy filter forward drop
+
+finish
